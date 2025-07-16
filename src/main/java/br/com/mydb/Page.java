f2508f87fc32342fdbfbf2c87fd276bfcd25fa2f
jonathan.silva <jonathan.msdev@gmail.com>
2025-07-15 22:08:35 -0300

    page class

---
+++ src/main/java/br/com/mydb/Page.java
@@ -0,0 +1,65 @@
+package br.com.mydb;
+
+import java.nio.ByteBuffer;
+
+public class Page {
+
+    private static final int PAGE_TYPE_OFFSET = 0;
+    private static final int ROW_COUNT_OFFSET = 1;
+
+    public static final int HEADER_SIZE = 3;
+
+    private final int pageNumber;
+    private final byte[] data;
+    private boolean isDirty;
+
+
+    public Page(int pageNumber, byte[] data) {
+        this.pageNumber = pageNumber;
+        this.data = data;
+        this.isDirty = false; // Uma página recém-lida do disco nunca está "suja".
+    }
+
+    public byte getPageType() {
+        return this.data[0];
+    }
+
+    public void setPageType(byte type) {
+        this.data[0] = type;
+    }
+
+    public int getRowCount() {
+        ByteBuffer buffer = ByteBuffer.wrap(data);
+        return buffer.getShort(ROW_COUNT_OFFSET);
+    }
+
+    public void setRowCount(int count) {
+        ByteBuffer buffer = ByteBuffer.wrap(data);
+        buffer.putShort(ROW_COUNT_OFFSET, (short) count);
+        this.isDirty = true;
+    }
+
+    public byte[] getRow(int slot) {
+        int offset = HEADER_SIZE + (slot * User.ROW_SIZE);
+        byte[] rowData = new byte[User.ROW_SIZE];
+        System.arraycopy(this.data, offset, rowData, 0, User.ROW_SIZE);
+        return rowData;
+    }
+
+    public void setRow(int slot, byte[] rowData) {
+        int offset = HEADER_SIZE + (slot * User.ROW_SIZE);
+        System.arraycopy(rowData, 0, this.data, offset, User.ROW_SIZE);
+    }
+
+    public int getPageNumber() {
+        return this.pageNumber;
+    }
+
+    public boolean isDirty() {
+        return this.isDirty;
+    }
+
+    public byte[] getBytes() {
+        return this.data;
+    }
+}
